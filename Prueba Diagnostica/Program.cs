a03a87031835fc3102081a0b871795a233b9f396
Jonathan Alejandro Hernández Cuevas <jonathan.hernandez1@virginiogomez.cl>
2022-07-08 16:47:11 -0400

    Funcionando Apertura y Maximo

--- Prueba Diagnostica/Program.cs
+++ Prueba Diagnostica/Program.cs
@@ -17,27 +17,122 @@ namespace Prueba_Diagnostica
         {
             StreamReader sr = new StreamReader("data.txt");
             string line = sr.ReadLine();
-            List<data> datos = new List<data>();
-           
+
+            //APERTURA MAXIMO  MINIMO CIERRE
+            double[] APERTURA = new double[200];
+            double[] MAXIMO = new double[200];
+            double[] MINIMO = new double[200];
+            double[] CIERRE = new double[200];
+
+            double[] APERTURAOR = new double[200];
+            double[] MAXIMOOR = new double[200];
+            double[] MINIMOOR = new double[200];
+            double[] CIERREOR = new double[200];
+
 
             int contador = 0;
             while (line != null)
             {
                 var dataLinea = line.Split(';');
-                
-                //*********Con lista de objetos
-                data item = new data();
-                item.APERTURA = double.Parse(dataLinea[0]);
-                item.MAXIMO = double.Parse(dataLinea[1]);
-                item.MINIMO = double.Parse(dataLinea[2]);
-                item.CIERRE = double.Parse(dataLinea[3]);
-                datos.Add(item);
+
+                //*********Con arreglos separados
+                APERTURA[contador] = double.Parse(dataLinea[0]);
+                MAXIMO[contador] = double.Parse(dataLinea[1]);
+                MINIMO[contador] = double.Parse(dataLinea[2]);
+                CIERRE[contador] = double.Parse(dataLinea[3]);
+
+                APERTURAOR[contador] = double.Parse(dataLinea[0]);
+                MAXIMOOR[contador] = double.Parse(dataLinea[1]);
+                MINIMOOR[contador] = double.Parse(dataLinea[2]);
+                CIERREOR[contador] = double.Parse(dataLinea[3]);
+
                 line = sr.ReadLine();
+
                 contador++;
             }
 
-            //MOSTRAR EL MAYOR VALOR DE CADA UNA DE LAS LISTAS.
-            buscaMayor(datos);
+
+            Array.Sort(APERTURAOR);
+            Array.Sort(MAXIMOOR);
+            Array.Sort(MINIMOOR);
+            Array.Sort(CIERREOR);
+
+
+
+            #region
+            int indice = 0;
+            int regresivamayor = 199;
+            bool op = true;
+
+
+            while (op)
+            {
+           
+                for (int i = 0; i < 200; i++)
+                {
+                
+                    if (APERTURA[i].Equals(APERTURAOR[regresivamayor]))
+                    {
+                        indice = i;
+                    }
+
+                }
+
+                int init = indice - 10;
+                int end = indice + 10;
+
+                if (APERTURA.Length > end && init > 0)
+                {
+                    Console.WriteLine("Cumple la condicion APERTURA" + APERTURA[indice]);
+                    
+                    op = false;
+                    break;
+                }
+                else
+                {
+                    regresivamayor = regresivamayor - 1;
+                    op = true;
+                }
+            }
+            #endregion
+
+
+            #region
+
+            op = true;
+
+            while (op)
+            {
+
+                for (int i = 0; i < 200; i++)
+                {
+
+                    if (MAXIMO[i].Equals(MAXIMOOR[regresivamayor]))
+                    {
+                        indice = i;
+                    }
+
+                }
+
+                int init = indice - 10;
+                int end = indice + 10;
+
+                if (MAXIMO.Length > end && init > 0)
+                {
+                    Console.WriteLine("Cumple la condicion MAXIMO" + MAXIMO[indice]);
+
+                    op = false;
+                    break;
+                }
+                else
+                {
+                    regresivamayor = regresivamayor - 1;
+                    op = true;
+                }
+            }
+            #endregion
+
+
 
 
             Console.ReadKey();
@@ -45,14 +140,7 @@ namespace Prueba_Diagnostica
 
         }
 
-        //Busca mayor en la lista de objetos.
-        public static void buscaMayor(List<data> datos)
-        {
-            foreach (var item in datos)
-            {
-                
-            }
-        }
+        
 
 
        
