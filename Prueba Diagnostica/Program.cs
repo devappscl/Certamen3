74bc1b7d5a62b5b7f8f850794874e12405d5c3d9
Jonathan Alejandro Hernández Cuevas <jonathan.hernandez1@virginiogomez.cl>
2022-07-08 12:49:39 -0400

    Actualización

--- Prueba Diagnostica/Program.cs
+++ Prueba Diagnostica/Program.cs
@@ -20,27 +20,27 @@ namespace Prueba_Diagnostica
             List<data> datos = new List<data>();
             data[] arrayData = new data[200];
             //APERTURA MAXIMO  MINIMO CIERRE
-            //double[] APERTURA = new double[200];
-            //double[] MAXIMO = new double[200];
-            //double[] MINIMO = new double[200];
-            //double[] CIERRE = new double[200];
+            double[] APERTURA = new double[200];
+            double[] MAXIMO = new double[200];
+            double[] MINIMO = new double[200];
+            double[] CIERRE = new double[200];
 
             int contador = 0;
             while (line != null)
             {
                 var dataLinea = line.Split(';');
                 //*********Con arreglo de objetos
-                arrayData[contador] = new data();
-                arrayData[contador].APERTURA = double.Parse(dataLinea[0]);
-                arrayData[contador].MAXIMO = double.Parse(dataLinea[1]);
-                arrayData[contador].MINIMO = double.Parse(dataLinea[2]);
-                arrayData[contador].CIERRE = double.Parse(dataLinea[3]);
+                //arrayData[contador] = new data();
+                //arrayData[contador].APERTURA = double.Parse(dataLinea[0]);
+                //arrayData[contador].MAXIMO = double.Parse(dataLinea[1]);
+                //arrayData[contador].MINIMO = double.Parse(dataLinea[2]);
+                //arrayData[contador].CIERRE = double.Parse(dataLinea[3]);
 
                 //*********Con arreglos separados
-                //APERTURA[contador] = double.Parse(dataLinea[0]);
-                //MAXIMO[contador] = double.Parse(dataLinea[1]);
-                //MINIMO[contador] = double.Parse(dataLinea[2]);
-                //CIERRE[contador] = double.Parse(dataLinea[3]);
+                APERTURA[contador] = double.Parse(dataLinea[0]);
+                MAXIMO[contador] = double.Parse(dataLinea[1]);
+                MINIMO[contador] = double.Parse(dataLinea[2]);
+                CIERRE[contador] = double.Parse(dataLinea[3]);
                 //*********Con lista de objetos
                 //data item = new data();
                 //item.APERTURA = double.Parse(dataLinea[0]);
@@ -51,23 +51,30 @@ namespace Prueba_Diagnostica
                 line = sr.ReadLine();
                 contador++;
             }
-            for (int i = 0; i < 200; i++)
-            {
 
 
-                Console.WriteLine(arrayData[i].APERTURA + " " + arrayData[i].MAXIMO + " " + arrayData[i].MINIMO + " " + arrayData[i].CIERRE);
-            }
+            //for (int i = 0; i < 200; i++)
+            //{
+            //    
+
+
+
+            //    Console.WriteLine(arrayData[i].APERTURA + " " + arrayData[i].MAXIMO + " " + arrayData[i].MINIMO + " " + arrayData[i].CIERRE);
+            //}
 
             //foreach (var item in datos)
             //{
             //    Console.WriteLine(item.APERTURA);
             //}
-            //for (int i = 0; i < 200; i++)
-            //{
-            //    Console.WriteLine(APERTURA[i]+" "+ MAXIMO[i]+" "+ MINIMO[i]+" "+ CIERRE[i]);
-            //}
-            Console.ReadKey();
 
+            for (int i = 0; i < 200; i++)
+            {
+                //Console.WriteLine(APERTURA[i] + " " + MAXIMO[i] + " " + MINIMO[i] + " " + CIERRE[i]);
+                
+
+            }
+
+            Console.ReadKey();
 
         }
     }
